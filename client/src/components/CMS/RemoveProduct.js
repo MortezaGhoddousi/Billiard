92203dd9af5ef87dd9c4bb4a264979eb5de36947
MortezaGhoddousi <djmgh1994@gmail.com>
2025-02-21 16:06:00 +0330

    updated server

--- client/src/components/CMS/RemoveProduct.js
+++ client/src/components/CMS/RemoveProduct.js
@@ -1,38 +1,42 @@
-import '../../css/AddProduct.css'
-import { useState } from 'react';
-import axios from 'axios';
-
+import "../../css/AddProduct.css";
+import { useState } from "react";
+import axios from "axios";
 
 function RemoveProduct() {
+    const [id, setId] = useState("");
 
-    const [id , setId] = useState ('');
-
-    function handleChange (e) {
-       setId(e.target.value);
+    function handleChange(e) {
+        setId(e.target.value);
     }
 
-    function handleSubmit (e) {
+    function handleSubmit(e) {
         e.preventDefault();
-        axios.delete('http://localhost:8000/api/delete/'+id)
-        .then((result) => {
-          console.log(result);
-        })
-        .catch((err) => {
-            console.log(err);
-        })
+        axios
+            .delete("https://billiard-server-puce.vercel.app/api/delete/" + id)
+            .then((result) => {
+                console.log(result);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
     }
 
-    return ( 
-        <section className='edit'>
+    return (
+        <section className="edit">
             <form onSubmit={handleSubmit}>
                 <h1>حذف محصول</h1>
                 <label>
-                    <input type="text" value={id} onChange={handleChange} placeholder="آیدی محصول برای حذف را وارد کنید"/>
+                    <input
+                        type="text"
+                        value={id}
+                        onChange={handleChange}
+                        placeholder="آیدی محصول برای حذف را وارد کنید"
+                    />
                 </label>
                 <input type="submit" value="ثبت" />
             </form>
         </section>
-     );
+    );
 }
 
 export default RemoveProduct;
